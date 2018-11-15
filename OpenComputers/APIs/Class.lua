d4ace6b1f40438d66e62d7dd46c0ae45e4e05f79
maanlamp <32102935+maanlamp@users.noreply.github.com>
2018-11-15 16:59:11 +0100

    add [API/Class & API/Inspect]

---
+++ OpenComputers/APIs/Class.lua
@@ -0,0 +1,54 @@
+local inspect = require("../Inspect.lua");
+
+local Class = {
+	private = {
+		className = "Class";
+	};
+	public = {};
+	toString = inspect;
+};
+Class.__index = Class;
+
+function Class.new (name, constructor)
+	local instance = {};
+	setmetatable(instance, Class);
+	instance.private.className = name;
+	instance.prototype = getmetatable(instance);
+	if constructor then
+		instance.constructor = constructor;
+		instance:constructor();
+	end
+	return instance;
+end
+
+--[[
+class Object {
+	constructor () {
+		this.foo = "bar";
+		this.baz = "quux";
+		this.test = {
+			hi = 3;
+			test = Class.new("Thing");
+		};
+	}
+
+	idk () {
+		-- Nothing
+	}
+}
+
+local object = new Object();
+]]--
+local object = Class.new("Object", function (this)
+	this.foo = "bar";
+	this.baz = "quux";
+	this.test = Class.new("Test", function (this)
+		this.hi = 3;
+		this.kont = Class.new("Kont", function (this)
+			this.isCool = true;
+		end);
+	end);
+	this.idk = function () end;
+end);
+
+print(object:toString());
